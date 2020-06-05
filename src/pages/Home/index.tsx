9136994ee03fc732bb2aa33b88d3b29267aa2ea0
drd-2rcl <drd.rclrd@gmail.com>
2020-06-04 23:14:01 -0300

    Add link in home to create point

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FiLogIn } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 import logo from "../../assets/logo.svg";
 import "./Home.css";
@@ -15,12 +16,12 @@ const Home = () => {
           <h1>Seu marketplace de coleta de resíduos</h1>
           <p>
             Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
-            <a href="/cadastro">
+            <Link to="/create-point">
               <span>
                 <FiLogIn />
               </span>
               <strong>Cadastre um ponto de coleta</strong>
-            </a>
+            </Link>
           </p>
         </main>
       </div>
